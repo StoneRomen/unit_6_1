b373363e13b64a22bb63d043aa07922ab6d5e690
StoneRomen <184683610+StoneRomen@users.noreply.github.com>
2024-10-29 20:44:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
 // Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
-function removeLeters(str, leters) {
-  let result = ''; 
+function removeLetters(str, letters) {
+  let result = '';
   for (let i = 0; i < str.length; i++) {
-    if (!leters.includes(str[i])) {
+    if (!letters.includes(str[i])) {
       result += str[i];
     }
-  } return console.log(result);
+  } return result;
 }
 
-removeLeters(" hello world", ['l', 'd']);
-removeLeters(" не хочу їсти", ['н', 'е']);
+let inputString = prompt('Введіть рядок');
+let lettersToRemove = prompt('Введіть символи для видалення').split('');
+console.log(removeLetters(inputString, lettersToRemove));
